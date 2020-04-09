5598b55074b19c9b5339034d779ac783bf787a1a
Matt Ross <mattross1080@gmail.com>
2020-04-09 17:12:37 -0400

    hi

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 'use strict';
-
+//https://stackoverflow.com/questions/16280747/sending-message-to-a-specific-connected-users-using-websocket
 const express = require('express');
 const { Server } = require('ws');
 console.log("Starting Server")
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
+var webSockets = {} // userID: webSocket
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
@@ -13,9 +14,22 @@ const server = express()
 const wss = new Server({ server });
 
 wss.on('connection', (ws) => {
+  var userID = parseInt(webSocket.upgradeReq.url.substr(1), 10)
   console.log('Client connected');
+  webSockets[userID] = webSocket
+  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(webSockets))
+
+
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
+    console.log('received from ' + userID + ': ' + message)
+    var messageArray = JSON.parse(message)
+    var toUserWebSocket = webSockets[messageArray[0]]
+    if (toUserWebSocket) {
+      console.log('sent to ' + messageArray[0] + ': ' + JSON.stringify(messageArray))
+      messageArray[0] = userID
+      toUserWebSocket.send(JSON.stringify(messageArray))
+    }
   })
   ws.on('close', () => console.log('Client disconnected'));
 });
