0a867fcb19a08f7043283085cd6179c13a017241
Matt Ross <mattross1080@gmail.com>
2020-11-13 13:13:47 -0500

    first version for node server

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const {
   Server
 } = require('ws');
 console.log("Starting Server")
+const url = require('url');
+
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 var webSockets = {} // userID: webSocket
@@ -26,78 +28,123 @@ class ConnectionManager {
     console.log("initializing class variables")
     this.websocketIds = []
     this.websockets = {}
+    this.count = 0;
+    this.current_index = 0;
     this.routeTable = {
-      "pause": {
-        "websocketIds": [],
-        "websockets": {}
-      },
-      "filterbubble": {
-        "websocketIds": [],
-        "websockets": {}
-      }
+
+      "websocketIds": [],
+      "websockets": {}
+
     }
+
+
   }
   init() {
 
 
     wss.on('connection', function connection(ws, req) {
+
+
+
+
+
+
       var userID = req.url.split("/")[1]
       console.log("Incoming url is " + req.url)
-      var route = req.url.split("/")[2]
+      // var route = req.url.split("/")[2]
       // console.log(ws)
       console.log('Client connected');
       console.log("User ID : " + userID)
+
       if (userID != null) {
 
-        this.routeTable[route]["websockets"][userID] = ws
-        this.routeTable[route]["websocketIds"].push(userID)
-        console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(this.routeTable[route]["websockets"]))
+        this.routeTable["websockets"][userID] = ws
+        this.routeTable["websocketIds"].push(userID)
+        console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(this.routeTable["websockets"]))
+        console.log("Web ids ",this.routeTable["websocketIds"])
         //
       } else {
         console.log("Could not process User ID")
       }
 
-      ws.on('message', function(message) {
+      this.routeTable["websockets"][userID].send("HELLO FROM SERVER")
+
+
+
+
+      ws.on('message', function (message) {
 
         try {
           console.log(`Received message => ${message}`)
           console.log('received from ' + userID + ': ' + message)
 
 
-          // var toUserWebSocket = this.routeTable[route]["websockets"][userID]
           // console.log(toUserWebSocket)
 
-          this.routeTable[route]["websocketIds"].forEach(function(clientId) {
+          this.routeTable["websocketIds"].forEach(function (clientId) {
             console.log(clientId)
             if (clientId != userID) {
               console.log("Sending to id " + clientId)
-              this.routeTable[route]["websockets"][clientId].send(message)
+              this.routeTable["websockets"][clientId].send(message)
             }
           }.bind(this))
-          // if (toUserWebSocket) {
-          //   console.log('sent to ' + messageArray[0] + ': ' + JSON.stringify(messageArray))
-          //   messageArray[0] = userID
-          //   toUserWebSocket.send(JSON.stringify(messageArray))
-          // }
+
         } catch (e) {
 
           console.log(e)
 
         }
       }.bind(this))
-      ws.on('close', function() {
+      ws.on('close', function () {
 
         console.log('Client disconnected : ' + userID)
-        this.routeTable[route]["websocketIds"] = this.routeTable[route]["websocketIds"].filter(e => e !== userID); // will return ['A', 'C']
-        console.log("Remaining Clients are : " + this.routeTable[route]["websocketIds"])
+        this.routeTable["websocketIds"] = this.routeTable["websocketIds"].filter(e => e !== userID); // will return ['A', 'C']
+        console.log("Remaining Clients are : " + this.routeTable["websocketIds"])
 
 
       }.bind(this));
     }.bind(this));
   }
+
+  broadcastMessages() {
+    // console.log(this.routeTable)
+
+    if (this.routeTable["websocketIds"].length > 0 ) {
+      console.log("current index", this.current_index)
+    var current_id = this.routeTable["websocketIds"][this.current_index]
+    console.log("current id ", current_id)
+    console.log("web socket ids",this.routeTable["websocketIds"])
+    this.routeTable["websockets"][current_id].send(this.count)
+    this.count++;
+    this.current_index++;
+
+    if (this.current_index > this.routeTable["websocketIds"].length -1) {
+      this.current_index = 0;
+    }
+
+    
+    // this.routeTable["websocketIds"].forEach(function (clientId) {
+    //   console.log(clientId)
+
+    //   this.routeTable["websockets"][clientId].send(this.count)
+    //   this.count++;
+
+    // }.bind(this))
+  }
+  } catch(e) {
+
+    console.log(e)
+
+  }
+
+
 }
 
 
 
 var connectionManager = new ConnectionManager()
 connectionManager.init()
+setInterval(function () {
+  connectionManager.broadcastMessages()
+}, 1000);
+
